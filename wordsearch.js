39cd82792d30c13582d3a5e93d60ded7927c6ca9
Zeinaaaa <zainabalbasuny8181@gmail.com>
2021-11-03 16:24:29 -0400

    fixing the word search pair programming challenge with @arie139

--- wordsearch.js
+++ wordsearch.js
@@ -1,8 +1,15 @@
+const transpose = matrix => matrix[0].map((r, i) => matrix.map(c => c[i]));
 const wordSearch = (letters, word) => { 
     const horizontalJoin = letters.map(ls => ls.join(''))
-    for (l of horizontalJoin) {
-        if (l.includes(word)) return true
+    let vertical = transpose(letters);
+    const verticalJoin = vertical.map(x => x.join(''))
+      if (horizontalJoin.includes(word)) {
+          return true;
+      } else if (verticalJoin.includes(word)) {
+           return true;
+      }else {
+            return false;
+      } 
     }
-}
 
 module.exports = wordSearch
